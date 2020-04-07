413fffa13be09b556c73326665f9b82a27075339
Abner Santana <FineLine>
2020-04-07 00:44:49 -0300

    commit

---
+++ ORIGAMID/4 - Numeros e Op/script.js
@@ -0,0 +1,31 @@
+
+// qual o resultado da seguinte expressao:
+var total = 10 + 5 + 2/2 + 10;
+console.log(total) // 26
+
+// crie duas expressos que retorne NaN
+num = 66;
+id = 'sat';
+result = num / id;
+console.log(result);
+// somar a string '200' com o numero '50' e retornar 250
+string1 = +'200';
+string2 = 50;
+result = string2 + string1;
+console.log(result);
+// incremente o numero 5 e retorne seu valor incrementado
+num = 5;
+num++;
+console.log(num);
+
+// como dividir o peso por 2?
+//var numero = '80';
+//var unidade = 'kg';
+//var peso = numero + unidade; // '80kg'
+//var pesoPorDois = peso / 2; // NaN (Not a Number)
+
+var numero = '80'/2; // dividir por 2 direto
+var unidade = 'kg';
+var peso = numero + unidade; // '40kg'
+
+console.log(peso)
