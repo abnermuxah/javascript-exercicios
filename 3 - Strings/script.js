3d15fb8f67cef694c76d7b1fc9dd801d40294933
Abner Santana <FineLine>
2020-04-07 00:04:48 -0300

    strings

---
+++ 3 - Strings/script.js
@@ -0,0 +1,12 @@
+// 1  -Declarar uma variavel contendo uma string
+var frase = "Estou aprendendo javascript";
+// 2 - Declarar uma variavem contendo um numero dentro de uma string
+var idade = '24';
+frase = `tenho ${idade} anos`;
+console.log(frase)
+// 3 - Declare uma variavel com sua idade (linha 4)
+// Declare duas variaveis uma com seu nome e outra com seu sobrenome depois some as mesmas
+var nome = "Abner";
+var sobrenome = "Santana";
+var nome_completo = nome + " " + sobrenome;
+console.log(nome_completo)
