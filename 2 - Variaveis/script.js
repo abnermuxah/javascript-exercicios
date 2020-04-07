b3bc7f69bdae05b7ac27f58941bb3ffb3076d6c3
Abner Santana <FineLine>
2020-04-06 23:30:36 -0300

    Initial commit

---
+++ 2 - Variaveis/script.js
@@ -0,0 +1,11 @@
+// 1 - Declarar uma variavel com seu nome:
+var nome = "Abner Santana";
+// 2 - Declarar uma Variavel com sua idade
+var idade = 17;
+// 3 - Declarar uma variavel com uma comida favorita e não atribuir valor
+let comida_favorita;
+// 4 - Atribuir valor a comida favorita
+comida_favorita = "Pizza";
+// 5 - Declarar 5 variaveis diferentes sem valores
+var peso, altura, sexo, estado_civil, data_nascimento;
+
